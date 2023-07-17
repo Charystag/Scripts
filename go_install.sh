a9853805c7be10fe76b502eead7a13a4aea5c503
nsainton <nsainton@student.42.fr>
2023-07-17 17:33:13 +0200

    Added variable in order to add more flexibility and automatism to the script

--- go_install.sh
+++ go_install.sh
@@ -1,6 +1,20 @@
+extension="tar.gz"
+source="go1.20.6.linux-"
+arm="$source""arm64"."$extension"
+amd="$source""amd64"."$extension"
+to_install=""
+if [ "$(uname -p)" = "aarch64" ]
+then
+    to_install=$arm
+else
+    to_install=$amd
+fi
+echo $to_install
+exit 1
 sudo -s -- <<INSTALL
-curl -fsSLO https://go.dev/dl/go1.20.6.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.6.linux-amd64.tar.gz
+curl -fsSLO https://go.dev/dl/$to_install
+rm -rf /usr/local/go && tar -C /usr/local -xzf $to_install
+rm -f $to_install
 INSTALL
 #Don't forget to add /usr/local/go/bin to the path
 #export PATH="$PATH:/usr/local/go/bin"
