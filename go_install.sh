60663333cc3d83abda563b1aaedd7fc3d97bb6ed
nsainton <nsainton@student.42.fr>
2023-07-17 17:14:36 +0200

    Added comment for PATH variable update

--- go_install.sh
+++ go_install.sh
@@ -2,3 +2,5 @@ sudo -s -- <<INSTALL
 curl -fsSLO https://go.dev/dl/go1.20.6.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.6.linux-amd64.tar.gz
 INSTALL
+#Don't forget to add /usr/local/go/bin to the path
+#export PATH="$PATH:/usr/local/go/bin"
