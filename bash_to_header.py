f8cbc7858e098d2c352df11f9360c52683891dcd
nsainton <nsainton@student.42.fr>
2023-08-05 15:56:52 +0200

    Script to convert bash_script into a format that can be written in C header file

---
+++ bash_to_header.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+import sys
+
+def to_header(line):
+	new_line = '\"'
+	for i in line :
+		if i == '\"' :
+			new_line += "\\\""
+		elif i == "\t" :
+			new_line += "\\t"
+		elif i == '\n' :
+			new_line += "\\n"
+		else :
+			new_line += i
+	new_line += '\" \\ \n'
+	return (new_line)
+
+arglen = len(sys.argv)
+if (arglen < 2):
+	print("Please give filename")
+	sys.exit(1)
+script_name = sys.argv[1]
+with open(script_name, "r") as script :
+	for line in script:
+		print(to_header(line), end = "")
