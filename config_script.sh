8c03d3cf06692dcd968c1f496d0a0aa8e5c34f11
nsainto <nsainton@student.42.fr>
2023-10-20 23:22:12 +0200

    feat(config_script): :rotating_light: Fixed linter warnings :rotating_light:

--- config_script.sh
+++ config_script.sh
@@ -25,7 +25,7 @@ then
 	exit 3
 fi
 
-sudo -s -- <<CONFIGURATION
+configuration="
 apt -y update
 apt -y upgrade
 apt -y install apt-file
@@ -44,22 +44,22 @@ apt -y install libbsd-dev
 apt -y install xclip
 apt -y install python3.10-venv
 apt -y install emacs
-CONFIGURATION
+"
 #The clang lines allows to install clang compiler and scan-build static llvm analyser
 #They seem to make the script buggy but feel free to install them by yourself
 #apt -y install clang
 #apt -y install clang-tools
 #These lines seems to make the configuration script buggy
-if [ $? != 0 ]
+if [ ! "$(sudo -s -- <<<"$configuration")" ]
 then
 	echo unknown error, might be due to interaction
 	exit 3
 fi
 lbin="$HOME/.local/bin"
 ubin="/usr/bin"
-mkdir -p $lbin
-ln -s $ubin/batcat $lbin/bat
-if [ -d $HOME/.oh-my-zsh ]
+mkdir -p "$lbin"
+ln -s "$ubin"/batcat "$lbin"/bat
+if [ -d "$HOME"/.oh-my-zsh ]
 then
 	echo "ZSH already exists"
 	omz_switch="off"
@@ -68,22 +68,22 @@ else
 	omz_switch="on"
 fi
 git config --global init.defaultBranch main
-git config --global user.email $2
-git config --global user.name $1
+git config --global user.email "$2"
+git config --global user.name "$1"
 git config --global core.editor "vim"
 python3 -m pip install --upgrade pip setuptools
 python3 -m pip install norminette
 if [ $omz_switch == "on" ]
 then
 	echo $omz_switch
-	cat <<-ALIASES >> $HOME/.zshrc
+	cat <<-ALIASES >> "$HOME"/.zshrc
 		alias wgcc="gcc -Wall -Wextra -Werror"
 		alias wclang="clang -Wall -Wextra -Werror"
 		alias norme="norminette -RCheckForbiddenSourceHeader"
 	ALIASES
-	echo "export PATH=\"\$HOME/.local/bin:\$PATH\"" >> $HOME/.zshrc
+	echo "export PATH=\"\$HOME/.local/bin:\$PATH\"" >> "$HOME"/.zshrc
 fi
-cat <<VIMCONFIG > $HOME/.vimrc
+cat <<VIMCONFIG > "$HOME"/.vimrc
 filetype on
 set autoindent
 set tabstop=4
@@ -95,9 +95,9 @@ VIMCONFIG
 
 plugins_folder="$HOME/.vim/plugin"
 stdheader="stdheader.vim"
-if [ ! -f $plugins_folder/stdheader.vim ]
+if [ ! -f "$plugins_folder"/stdheader.vim ]
 then
-	mkdir -p $plugins_folder
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/42Paris/42header/master/plugin/stdheader.vim -o $plugins_folder/$stdheader)"\
+	mkdir -p "$plugins_folder"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/42Paris/42header/master/plugin/stdheader.vim -o "$plugins_folder"/"$stdheader")"\
 	&& echo $stdheader installed || echo Unknown error
 fi
