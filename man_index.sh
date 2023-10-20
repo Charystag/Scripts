dabb73abd2eda0ff2fddcc5723e85bc8bcf61152
nsainto <nsainton@student.42.fr>
2023-10-20 23:31:07 +0200

    feat(man_index): :rotating_light: Fixed linter warnings :rotating_light:

--- man_index.sh
+++ man_index.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-mkdir -p man_index ; for i in {1..8} ; do apropos '.*' | grep -E ".*\($i\).*" 1>man_index/man$i\_index ; done
+#!/bin/bash
+man_index_dir="man_index"
+mkdir -p "$man_index_dir"
+for i in {1..8}
+do apropos '.*' | grep -E ".*\($i\).*" 1>"$man_index_dir"/man"$i"_index
+done
