e070d87cd3b43023e3891906335598b2e1a37cfe
nsainto <nsainton@student.42.fr>
2023-10-23 23:35:10 +0200

    feat(42projects): :fire: Removed useless scripts :fire:

--- 42projects/norm_check.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-norm_check()
-{
-	echo bonjour
-	printf "\033[23Global warming"
-	echo bonjour
-	echo salut
-}
--- 42projects/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-if declare -f "$1" > /dev/null
-then
-	"$@"
-else
-	echo "'$1' is not a function name" >&2
-	exit 1
-fi
