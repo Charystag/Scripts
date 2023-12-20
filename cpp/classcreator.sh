3930955a5764ebe686cb3d93d370e75c42484369
lazydev <nsainton@student.42.fr>
2023-12-20 04:37:02 +0100

    feat(classcreator): :sparkles: Started implementing creation of .h file :sparkles:

--- cpp/classcreator.sh
+++ cpp/classcreator.sh
@@ -1,36 +1,75 @@
 #!/usr/bin/env sh
 
 usage(){
-	cat <<-"USAGE"
-	Usage: classcreator.sh classname [dir] [header_dir class_dir]
+	declare -I name
+
+	cat <<-USAGE
+	Usage: $name classname [dir] [header_dir class_dir]
 	Examples:
-		-classcreator.sh classname -> creates a classname.cpp and classname.h class file in the current directory
-		-classcreator.sh classname dir -> creates a dir/classname.cpp and dir/classname.h 
+		-$name classname -> creates a classname.cpp and classname.h class file in the current directory
+		-$name classname dir -> creates a dir/classname.cpp and dir/classname.h 
 		class file in the specified directory
-		-classcreator.sh classname header_dir class_dir -> creates a header_dir/classcreator.h and a 
+		-$name classname header_dir class_dir -> creates a header_dir/classcreator.h and a 
 		class_dir/classname.cpp file in the specified directories for header files and source files
 	USAGE
 }
 
+:<<-"GETNAME"
+	Function that gets the name of the script that should be used for the printing messages
+GETNAME
 getname(){
-	if echo "$0" | grep -E "^/proc/self/fd" 1>/dev/null
-	then
-		name="classcreator"
-	else
-		name="$0"
-	fi
+	if echo "$0" | grep -E "^/proc/self/fd" 1>/dev/null;then name="classcreator"
+	else name="$(basename -s .sh "$0")"; fi
+}
+
+:<<-CREATE_FILE
+	Function that creates a file with the given filename as an input. If no file is provided, do nothing
+	If the file can't be created, return an error
+CREATE_FILE
+create_file(){
+	if [ "$1" = "" ] ; then return 1; fi
+	if [ -f "$1" ] ; then shred -f -n 10 -u -z "$1"; fi
+	if ! touch "$1" ; then echo "Couldn't create file : $1"; return 1;fi
+	return 0;
+}
+
+:<<-"CREATE_HEADER"
+CREATE_HEADER
+create_header(){
+	local header
+	local upp
+	local low
+
+	if [ "$1" = "" ] ; then return 1 ; fi
+	class="$1"
+	header="$class".h
+	upp="$(echo "$class" | tr [:lower:] [:upper:])"
+	if ! create_file "$header" ; then return 1 ; fi
+	cat >"$header" <<HEADER
+#ifndef __$upp\\_H__
+# define __$upp\\_H__
+class	$class\{
+	$class();
+	$class( const $class & );
+	$class& operator=( const $class & );
+	virtual ~$class();
+}
+#endif
+HEADER
+	return 0
 }
 
 main(){
 	local name
 
+	getname
 	if [ "$1" = "" ]
 	then
 		usage
 		return 0
 	fi
-	getname
 	echo "My name is : $name"
+	create_header "$1"
 }
 
 main "$@"
